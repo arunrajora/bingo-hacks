c404fb5a7c40a42fa332d5c3299c54260ba4df81
arunrajora <rajora.rajora2@outlook.com>
2017-03-26 14:36:17 +0530

    add blockchain demo.

---
+++ blockchain-demo/public/javascripts/fireb.js
@@ -0,0 +1,10 @@
+var config = {
+   apiKey: "AIzaSyA3CLj0aKmciZv1fmWSk1oVi4s2Nw7aiU0",
+   authDomain: "bingo-1f719.firebaseapp.com",
+   databaseURL: "https://bingo-1f719.firebaseio.com",
+   storageBucket: "bingo-1f719.appspot.com",
+   messagingSenderId: "1443058307"
+ };
+ $(document).ready(function(){
+	 firebase.initializeApp(config);
+ });
